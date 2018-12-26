715d50b3b47a6fe2a50d9706809ab86a36236a02
An Minh <trinhminhan1996@gmail.com>
2018-12-26 23:02:22 +0700

    dashboard

---
+++ QuanLyCongVan/App_Start/Bundle/AdminBundle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace QuanLyCongVan.App_Start.Bundle
+{
+    public class AdminBundle
+    {
+        public static BundleCollection RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/public/js/admin/login").Include(
+                "~/public/assets/jquery/dist/jquery.js",
+                "~/public/js/common/common.js",
+                "~/public/js/common/jquery.alerts.js",
+                "~/public/assets/bootstrap/dist/js/bootstrap.js",
+                "~/public/js/admin/adminlogin/index.js",
+                "~/public/js/common/base64.js",
+                "~/public/js/common/md5.js",
+                "~/public/js/common/message.js",
+                "~/public/js/common/jquery.validate.js",
+                "~/public/js/common/jquery.validate.unobtrusive.js",
+                "~/public/js/common/jquery.validate-vsdoc.js",
+                "~/public/js/common/jquery-cookie.js",
+                "~/public/js/common/jquery.error-style.js",
+                "~/public/js/common/jquery.alerts.js"
+            ));
+            bundles.Add(new StyleBundle("~/public/css/admin/login").Include(
+                "~/public/assets/bootstrap/dist/css/bootstrap.css",
+                "~/public/assets/font-awesome-4.7.0/css/font-awesome.css",
+                "~/public/css/admin/nprogress.css",
+                "~/public/assets/animate.css/animate.css",
+                "~/public/css/admin/custom.css",
+                "~/public/css/common/error-notify.css"
+            ));
+            return bundles;
+        }
+    }
+}
