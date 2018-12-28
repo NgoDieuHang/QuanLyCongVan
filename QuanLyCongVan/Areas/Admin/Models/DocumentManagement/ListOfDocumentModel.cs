0ee8fde47d586d0eebf44cc77cad2629ecf288e4
Minh Hoang <minhhoang97hk@gmail.com>
2018-12-28 14:59:32 +0700

    hiển thị danh sách loai van ban va xoa

---
+++ QuanLyCongVan/Areas/Admin/Models/DocumentManagement/ListOfDocumentModel.cs
@@ -0,0 +1,70 @@
+﻿using QLCV.Common;
+using QLCV.Database;
+using QuanLyCongVan.Areas.Admin.Models.DocumentManagement.Schema;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Z.EntityFramework.Plus;
+
+namespace QuanLyCongVan.Areas.Admin.Models.DocumentManagement
+{
+    public class ListOfDocumentModel
+    {
+        DataContext context;
+        public ListOfDocumentModel()
+        {
+            context = new DataContext();
+        }
+
+        public List<Document> GetListOfDocument()
+        {
+            try
+            {
+
+
+                List<Document> listOfDocument = new List<Document>();
+
+                //.Where(x => !x.DelFlag)
+                listOfDocument = context.LoaiVanBans
+                    .Select(x => new Document
+                    {
+                        Id = x.Id,
+                        tenLoaiVanBan = x.TenLoaiVanBan,
+                        kyHieu = x.KiHieu
+                    }).ToList();
+
+                return listOfDocument;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Xóa các loại văn bản trong DB.
+        /// Author       :   HoangNM - 27/12/2018 - create
+        /// </summary>
+        /// <param name="ids">Danh sách id của các loại văn bản sẽ xóa</param>
+        /// <returns>True nếu xóa thành công, False nếu không còn hình ảnh được hiển thị trên trang chủ, Excetion nếu có lỗi</returns>
+        public bool DeleteDocument(List<int> ids)
+        {
+            DbContextTransaction transaction = context.Database.BeginTransaction();
+            try
+            {
+                context.LoaiVanBans.Where(x => ids.Contains(x.Id)).Delete();
+                context.SaveChanges();
+
+                transaction.Commit();
+                return true;
+            }
+            catch (Exception e)
+            {
+                transaction.Rollback();
+                throw e;
+            }
+        }
+    }
+}
