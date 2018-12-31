43618ba423c3fa293e72779e0bf1b112f0c2efd0
dieuhang96.bk <dieuhang96.bk@gmail.com>
2018-12-31 17:30:29 +0700

    commit Organization

--- QuanLyCongVan/Areas/Admin/Controllers/OrganizationManagementController.cs
+++ QuanLyCongVan/Areas/Admin/Controllers/OrganizationManagementController.cs
@@ -1,17 +1,153 @@
-﻿using System;
+﻿using QLCV.Common;
+using QuanLyCongVan.Areas.Admin.Models.OrganizationManagement;
+using QuanLyCongVan.Areas.Admin.Models.OrganizationManagement.Schema;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using static QLCV.Common.Enum.ConstantsEnum;
+using static QLCV.Common.Enum.MessageEnum;
 
 namespace QuanLyCongVan.Areas.Admin.Controllers
 {
     public class OrganizationManagementController : Controller
     {
         // GET: Admin/OrganizationManagement
-        public ActionResult Index()
+        /// <summary>
+        /// Điều hướng đến trang hiển thị danh sách Organization nếu là request thông thường.
+        /// Trả về table chứa danh sách Organization nếu là Ajax.
+        /// Điều hướng về trang lỗi nếu có lỗi sảy ra.
+        /// Author       :   HangNTD - 02/08/2018 - create
+        /// </summary>
+        /// <param name="condition">Đối tượng chứa điều kiện tìm kiếm, tạo thành từ query string</param>
+        /// <returns>
+        /// Trang danh sách Organization.
+        /// Partial view chứa table danh sách Organization.
+        /// </returns>
+        /// <remarks>
+        /// Method: GET
+        /// RouterName: ListOfOrganization
+        /// </remarks>
+        public ActionResult ListOfOrganization(OrganizationConditionSearch condition)
         {
-            return View();
+            try
+            {
+                if (Request.IsAjaxRequest())
+                {
+                    return View("Partial/_tableOfOrganization", new OrganizationModel().GetListOfOrganization(condition));
+                }
+                return View(new OrganizationModel().GetListOfOrganization(condition));
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("Error", "Error", new { area = "error", error = e.Message });
+            }
+        }
+        /// <summary>
+        /// Xóa các Organization theo danh sách id Organization được gửi lên.
+        /// Author       :   HangNTD - 02/08/2018 - create
+        /// </summary>
+        /// <param name="ids">Danh sách id các Organization sẽ xóa</param>
+        /// <returns>Đối tượng chứa thông tin về quá tringh xóa Organization</returns>
+        /// <remarks>
+        /// Method: POST
+        /// RouterName: DeleteOrganizations
+        /// </remarks>
+        public JsonResult DeleteOrganizations(List<int> ids)
+        {
+            ResponseInfo response = new ResponseInfo();
+            try
+            {
+                bool deleted = new OrganizationModel().DeleteOrganizations(ids);
+                if (!deleted)
+                {
+                    response.Code = 201;
+                    response.MsgNo = 45;
+                }
+            }
+            catch (Exception e)
+            {
+                response.Code = (int)CodeResponse.ServerError;
+                response.MsgNo = (int)MsgNO.ServerError;
+                response.ThongTinBoSung1 = e.Message;
+            }
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// Trả về view add Organization khi click add Organization.
+        /// Author       :   HangNTD - 02/08/2018 - create
+        /// </summary>
+        /// </remarks>
+        public ActionResult ViewCreateOrganization()
+        {
+            try
+            {
+                return View("OrganizationMaster");
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("Error", "Error", new { area = "error", error = e.Message });
+            }
+        }
+        /// <summary>
+        /// Điều hướng đến trang view sửa Organization
+        /// Author       :   HangNTD - 25/07/2018 - create
+        /// </summary>
+        /// <param name="id">Đối tượng chứa thông tin về quá trình chỉnh sửa thông tin</param>
+        /// <returns>Trả về trang view sửa Organization</returns>
+        /// <remarks>
+        /// Method: POST
+        /// RouterName: ViewEditOrganization
+        /// </remarks>
+        public ActionResult ViewEditOrganization(string id)
+        {
+            try
+            {
+                OrganizationMaster OrganizationMaster = new OrganizationModel().GetOrganization(id);
+                if (OrganizationMaster.Mode == (int)ModeMaster.Insert)
+                {
+                    return RedirectToAction("ViewCreateOrganization");
+                }
+                return View("OrganizationMaster", OrganizationMaster);
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("Error", "Error", new { area = "error", error = e.Message });
+            }
+        }
+        /// <summary>
+        /// Tạo Organization hoặc cập nhật Organization theo thông tin người dùng đưa lên.
+        /// Author       :   HangNTD - 25/07/2018 - create
+        /// </summary>
+        /// <param name="data">Thông tin Organization mà người dùng nhập vào</param>
+        /// <returns>Chuỗi Json chứa kết quả của việc tạo hoặc cập nhật Organization</returns>
+        /// <remarks>
+        /// Method: POST
+        /// RouterName: SaveOrganization
+        /// </remarks>
+        [HttpPost]
+        public JsonResult SaveOrganization(Organization data)
+        {
+            ResponseInfo response = new ResponseInfo();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //response = new OrganizationModel().SaveOrganization(data);
+                }
+                else
+                {
+                    response.Code = (int)CodeResponse.NotValidate;
+                    response.ListError = ModelState.GetModelErrors();
+                }
+            }
+            catch (Exception e)
+            {
+                response.Code = (int)CodeResponse.ServerError;
+                response.MsgNo = (int)MsgNO.ServerError;
+                response.ThongTinBoSung1 = e.Message;
+            }
+            return Json(response, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- QuanLyCongVan/Areas/Admin/Models/OrganizationManagement/OrganizationModel.cs
+++ QuanLyCongVan/Areas/Admin/Models/OrganizationManagement/OrganizationModel.cs
@@ -2,7 +2,11 @@
 using QLCV.Database;
 using QuanLyCongVan.Areas.Admin.Models.OrganizationManagement.Schema;
 using System;
+using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using Z.EntityFramework.Plus;
+using static QLCV.Common.Enum.ConstantsEnum;
 using TblOrganization = QLCV.Database.CoQuanBanHanh;
 
 namespace QuanLyCongVan.Areas.Admin.Models.OrganizationManagement
@@ -20,8 +24,9 @@ namespace QuanLyCongVan.Areas.Admin.Models.OrganizationManagement
         DataContext context;
         public OrganizationModel()
         {
-            context=new DataContext();
+            context = new DataContext();
         }
+
         /// <summary>
         /// Tìm kiếm các organization theo điều kiện cho trước.
         /// Author       :   HangNTD - 27/12/2018 - create
@@ -55,8 +60,8 @@ namespace QuanLyCongVan.Areas.Admin.Models.OrganizationManagement
                     .Take(listOfOrganization.Paging.NumberOfRecord).Select(x => new Organization
                     {
                         Id = x.Id,
-                        TenCoQuanBanHanh=x.TenCoQuanBanHanh,
-                        KiHieu=x.KiHieu
+                        TenCoQuanBanHanh = x.TenCoQuanBanHanh,
+                        KiHieu = x.KiHieu
                     }).ToList();
                 listOfOrganization.Condition = condition;
                 return listOfOrganization;
@@ -73,11 +78,11 @@ namespace QuanLyCongVan.Areas.Admin.Models.OrganizationManagement
         /// </summary>
         /// <param name="id">id của Organization sẽ được hiển thị</param>
         /// <returns>Thông tin của Organization</returns>
-        public Organization GetOrganization(string id)
+        public OrganizationMaster GetOrganization(string id)
         {
             try
             {
-                Organization organization = new Organization();
+                OrganizationMaster organizationMaster = new OrganizationMaster();
                 int idOrganization = 0;
                 try
                 {
@@ -87,20 +92,117 @@ namespace QuanLyCongVan.Areas.Admin.Models.OrganizationManagement
                 TblOrganization tblOrganization = context.CoQuanBanHanhs.FirstOrDefault(x => x.Id == idOrganization && !x.DelFlag);
                 if (tblOrganization != null)
                 {
-                    organization.Id = tblOrganization.Id;
-                    organization.TenCoQuanBanHanh = tblOrganization.TenCoQuanBanHanh;
-                    organization.KiHieu = tblOrganization.KiHieu;
+                    organizationMaster.Mode = (int)ModeMaster.Update;
+                    organizationMaster.Organization.Id = tblOrganization.Id;
+                    organizationMaster.Organization.TenCoQuanBanHanh = tblOrganization.TenCoQuanBanHanh;
+                    organizationMaster.Organization.KiHieu = tblOrganization.KiHieu;
                 }
                 else
                 {
                     return null;
                 }
-                return organization;
+                return organizationMaster;
             }
             catch (Exception e)
             {
                 throw e;
             }
         }
+
+        /// <summary>
+        /// Xóa các Organization trong DB.
+        /// Author       :   HangNTD - 02/08/2018 - create
+        /// </summary>
+        /// <param name="ids">Danh sách id của các Organization sẽ xóa</param>
+        /// <returns>True nếu xóa thành công, False nếu không còn Organization được hiển thị trên trang chủ, Excetion nếu có lỗi</returns>
+        public bool DeleteOrganizations(List<int> ids)
+        {
+            DbContextTransaction transaction = context.Database.BeginTransaction();
+            try
+            {
+                bool result = true;
+                if (context.CoQuanBanHanhs.Count(x => !ids.Contains(x.Id) && !x.DelFlag) > 1)
+                {
+                    context.CoQuanBanHanhs.Where(x => ids.Contains(x.Id) && !x.DelFlag).Update(x => new TblOrganization
+                    {
+                        DelFlag = true
+                    });
+                    context.SaveChanges();
+                }
+                else
+                {
+                    result = false;
+                }
+                transaction.Commit();
+                return result;
+            }
+            catch (Exception e)
+            {
+                transaction.Rollback();
+                throw e;
+            }
+        }
+        /// <summary>
+        /// Cập nhật lại  Organization từ thông tin mà người dùng gửi lên
+        /// Author       :   HangNTD - 17/08/2018 - create
+        /// </summary>
+        /// <param name="course">Các thông tin của Organization muốn cập nhật</param>
+        /// <returns>Thông tin về việc cập nhật Organization thành công hay thất bại</returns>
+        public ResponseInfo UpadateOrganization(Organization organization)
+        {
+            DbContextTransaction transaction = context.Database.BeginTransaction();
+            try
+            {
+                ResponseInfo response = new ResponseInfo();
+                TblOrganization tblOrganization = context.CoQuanBanHanhs.Where(x => x.Id == organization.Id && !x.DelFlag).FirstOrDefault();
+                tblOrganization.TenCoQuanBanHanh = organization.TenCoQuanBanHanh;
+                tblOrganization.KiHieu = organization.KiHieu;
+                context.SaveChanges();
+                transaction.Commit();
+                return response;
+            }
+            catch (Exception e)
+            {
+                transaction.Rollback();
+                throw e;
+            }
+        }
+        /// <summary>
+        /// Tạo  Organization từ thông tin mà người dùng gửi lên
+        /// Author       :   HangNTD - 17/08/2018 - create
+        /// </summary>
+        /// <param name="course">Các thông tin của Organization muốn tạo</param>
+        /// <returns>Thông tin về việc tạo  Organization thành công hay thất bại</returns>
+        public ResponseInfo AddOrganization(Organization organization)
+        {
+            DbContextTransaction transaction = context.Database.BeginTransaction();
+            try
+            {
+                ResponseInfo response = new ResponseInfo();
+                organization.Id = context.CoQuanBanHanhs.Count() == 0 ? 1 : context.CoQuanBanHanhs.Max(x => x.Id) + 1;
+                context.CoQuanBanHanhs.Add(new TblOrganization
+                {
+                    Id = organization.Id,
+
+                });
+                context.CoQuanBanHanhs.Add(new TblOrganization
+                {
+                    Id = organization.Id,
+                    TenCoQuanBanHanh = organization.TenCoQuanBanHanh,
+                    KiHieu = organization.KiHieu
+                });
+                context.SaveChanges();
+                response.ThongTinBoSung1 = organization.Id + "";
+
+                transaction.Commit();
+                return response;
+            }
+            catch (Exception e)
+            {
+                transaction.Rollback();
+                throw e;
+            }
+        }
+
     }
 }
